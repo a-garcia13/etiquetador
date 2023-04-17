bf419d4ec5b22f40395fd2c0755bc9dc118f7316
Alejandro García Flores <a.garcia13@uniandes.edu.co>
2023-04-16 21:08:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pandas as pd
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import random
-
+import threading
+import queue
+import time
 # Connect to the MongoDB database and get the collection
 client = MongoClient(
     "mongodb://inst-newstic:7E69wh96tzcKjK5u3tnFHK7BwbpT2dbU61JsXxVsYdPNTuazAGNBZQPxNo6xaQcDJbxlsIKmiDrhACDbDy1fmg%3D%3D@inst-newstic.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@inst-newstic@")
@@ -24,6 +26,8 @@ if 'num' not in st.session_state:
 if 'data' not in st.session_state:
     st.session_state.data = []
 
+# Create a queue for updates
+update_queue = queue.Queue()
 
 # Get a random news article from the MongoDB collection
 def get_random_news():
@@ -37,11 +41,21 @@ def update_news_status(news_id, answer):
     to_update = collection.find_one({"_id": ObjectId(news_id)})
     if "es_economica_manual" not in to_update:
         collection.update_one({"_id": ObjectId(news_id)}, {"$set": {"es_economica_manual": answer}})
-    else:
-        with st.spinner("Actualizando collección..."):
-            cursor = collection.find(query)
-            df = pd.DataFrame(list(cursor))
 
+def refresh_dataframe():
+    global df
+    cursor = collection.find(query)
+    df = pd.DataFrame(list(cursor))
+
+def process_updates():
+    while True:
+        news_id, answer = update_queue.get()
+        update_news_status(news_id, answer)
+        update_queue.task_done()
+
+# Start a background thread to process updates
+update_thread = threading.Thread(target=process_updates, daemon=True)
+update_thread.start()
 
 class NewArticle:
     def __init__(self, article, id):
@@ -56,6 +70,12 @@ def main():
     placeholder2 = st.empty()
 
     while True:
+        current_time = time.time()
+        if current_time - last_refresh_time >= 1800:  # 3600 seconds = 1 hour
+            with st.spinner("Refreshing the data..."):
+                refresh_dataframe()
+                last_refresh_time = current_time
+
         num = st.session_state.num
 
         if placeholder2.button('end', key=num):
@@ -70,8 +90,7 @@ def main():
                     article = get_random_news()
                 news = NewArticle(article['Desc_Noticia'], article['_id'])
                 if news.yes_button or news.no_button:
-                    with st.spinner("Actualizando base de datos..."):
-                        update_news_status(article["_id"], "Sí" if news.yes_button else "No")
+                    update_queue.put((article["_id"], "Sí" if news.yes_button else "No"))
                     st.session_state.data.append({
                         '_id': article['_id'], 'es_economica_manual': "Sí" if news.yes_button else "No"})
                     st.session_state.num += 1
@@ -79,7 +98,6 @@ def main():
                     placeholder2.empty()
                 else:
                     st.stop()
-
 
 st.title("Etiquetador de noticias")
 st.write("Seleccione 'Sí', si la noticia es económica, de lo contrario seleccione 'No'.")
